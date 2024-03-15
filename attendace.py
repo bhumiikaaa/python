1aa9e4fdb390f06f2734e89ac0b4e0e55d48cd3c
bhumiikaaa <163533269+bhumiikaaa@users.noreply.github.com>
2024-03-15 17:44:57 +0530

    Delete attendace.py

--- attendace.py
+++
@@ -1,8 +0,0 @@
-"""attendance=int(input("Enter your attendane:"))
-if attendance>75:
-    print("Attendance is good")
-elif attendance>30:
-    print("Attendance is bad")
-else:
-    print("You are removed from the cllg")"""
-    
