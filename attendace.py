4a7cd7b80d9c3332ed48a8108fb4cc1b92e1c53e
bhumiikaaa <bhumikabandkar@gmail.com>
2024-03-15 17:29:34 +0530

    first

---
+++ attendace.py
@@ -0,0 +1,8 @@
+"""attendance=int(input("Enter your attendane:"))
+if attendance>75:
+    print("Attendance is good")
+elif attendance>30:
+    print("Attendance is bad")
+else:
+    print("You are removed from the cllg")"""
+    
---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Hello World")
+numbers=[]
+n=int(input("Length of list: "))
+for i in range(1,n+1):
+    num=int(input("Enter number"+str(i)+":"))
+    numbers.append(num)
+    
+numbers.sort()
+sorted_list=(sorted(numbers))
+print("The second largest number is:" + str(sorted_list[-2]))
